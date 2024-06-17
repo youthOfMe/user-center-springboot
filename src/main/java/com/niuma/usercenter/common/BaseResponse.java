f230c252e6784e6718499146e806e6e549592062
youthOfMe <2040690180@qq.com>
2024-06-17 12:28:40 +0800

    feat: 初步封装异常处理

---
+++ src/main/java/com/niuma/usercenter/common/BaseResponse.java
@@ -0,0 +1,8 @@
+package com.niuma.usercenter.common;
+
+/**
+ * 作者：洋哥
+ * 描述：牛逼
+ */
+public class BaseResponse {
+}
---
+++ src/main/java/com/niuma/usercenter/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.niuma.usercenter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户接口
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
