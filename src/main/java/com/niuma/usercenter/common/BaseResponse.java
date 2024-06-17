ab1f0a3f8dd7b893ec7ea0edf8ec3f4315236bbf
youthOfMe <2040690180@qq.com>
2024-06-17 12:34:46 +0800

    feat: 封装统计响应类

--- src/main/java/com/niuma/usercenter/common/BaseResponse.java
+++ src/main/java/com/niuma/usercenter/common/BaseResponse.java
@@ -1,8 +1,36 @@
 package com.niuma.usercenter.common;
 
-/**
- * 作者：洋哥
- * 描述：牛逼
- */
-public class BaseResponse {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class BaseResponse<T> implements Serializable {
+
+    private int code;
+
+    private T data;
+
+    private String message;
+
+    private String description;
+
+    public BaseResponse(int code, T data, String message, String description) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+        this.description = description;
+    }
+
+    public BaseResponse(int code, T data, String message) {
+        this(code, data, message, "");
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "", "");
+    }
+
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription())
+    }
 }
