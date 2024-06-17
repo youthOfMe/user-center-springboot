e6954741e8e771ab86995e9fe293485f031f124a
youthOfMe <2040690180@qq.com>
2024-06-17 12:47:30 +0800

    feat: 完成封装异常处理类

--- src/main/java/com/niuma/usercenter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/niuma/usercenter/exception/GlobalExceptionHandler.java
@@ -1,14 +1,25 @@
 package com.niuma.usercenter.exception;
 
 import com.niuma.usercenter.common.BaseResponse;
+import com.niuma.usercenter.common.ErrorCode;
+import com.niuma.usercenter.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BusinessException.class)
     public BaseResponse businessExceptionHandler(BusinessException exception) {
         log.error("businessException: " + exception.getMessage());
+        return ResultUtils.error(exception.getCode(), exception.getMessage(), exception.getDescription());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse businessExceptionHandler(RuntimeException exception) {
+        log.error("runtimeException: " + exception.getMessage());
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, exception.getMessage(), "");
     }
 }
