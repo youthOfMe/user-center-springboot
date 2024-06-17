52c8f4459ef4d3cee18ed17f87cdacb73e1cdfd1
youthOfMe <2040690180@qq.com>
2024-06-17 12:43:59 +0800

    feat: 封装基础响应工具类

--- src/main/java/com/niuma/usercenter/common/BaseResponse.java
+++ src/main/java/com/niuma/usercenter/common/BaseResponse.java
@@ -31,6 +31,6 @@ public class BaseResponse<T> implements Serializable {
     }
 
     public BaseResponse(ErrorCode errorCode) {
-        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription())
+        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription());
     }
 }
--- src/main/java/com/niuma/usercenter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/niuma/usercenter/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.niuma.usercenter.exception;
 
+import com.niuma.usercenter.common.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    public BaseResponse businessExceptionHandler(BusinessException exception) {
+        log.error("businessException: " + exception.getMessage());
+    }
 }
