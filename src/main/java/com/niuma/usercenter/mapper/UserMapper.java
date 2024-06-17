a6ec06bb1a707d5e4ffa453532040a8008432640
youthOfMe <2040690180@qq.com>
2024-06-17 11:18:31 +0800

    feat: 重构User和UserMapper

--- src/main/java/com/niuma/usercenter/mapper/UserMapper.java
+++ src/main/java/com/niuma/usercenter/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package com.niuma.usercenter.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.niuma.usercenter.model.User;
+import com.niuma.usercenter.model.domain.User;
 
 public interface UserMapper extends BaseMapper<User> {
 
--- src/test/java/com/niuma/usercenter/UsercenterApplicationTests.java
+++ src/test/java/com/niuma/usercenter/UsercenterApplicationTests.java
@@ -1,13 +1,25 @@
 package com.niuma.usercenter;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UsercenterApplicationTests {
 
     @Test
-    void contextLoads() {
+    @BeforeAll
+    public static void contextLoads() {
+        System.out.println("执行全部");
     }
 
+    @Test
+    @BeforeEach
+    void niuma() {
+        System.out.println("我是牛马");
+    }
+
+    @Test
+    void common() {
+        System.out.println("我是普通的方法");
+    }
 }
